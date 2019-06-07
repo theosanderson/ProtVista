8b8187c293f89482771ce15cbff50d14be93aed5
Max Bileschi <mlbileschi@gmail.com>
2019-06-07 15:02:38 -0400

    Wider track labels by default

--- src/FeaturesViewer.js
+++ src/FeaturesViewer.js
@@ -460,7 +460,7 @@ var FeaturesViewer = function(opts) {
     fv.dispatcher = d3.dispatch("featureSelected", "featureDeselected", "ready", "noDataAvailable", "noDataRetrieved",
         "notFound", "notConfigRetrieved", "regionHighlighted");
 
-    fv.width = 708;
+    fv.width = 648;
     fv.maxZoomSize = 30;
     fv.selectedFeature = undefined;
     fv.selectedFeatureElement = undefined;
